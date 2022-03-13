4d75a91e341475bfef2df09ca7a1742c0475fe43
jaouad <Jadsarouti@outlook.com>
2022-03-13 18:56:07 -0400

    Adding a data provider | check

--- src/test/java/TestCases/LoginTest.java
+++ src/test/java/TestCases/LoginTest.java
@@ -1,6 +1,7 @@
 package TestCases;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pages.LoginPage;
@@ -11,16 +12,26 @@ public class LoginTest extends BaseClass{
 	// to be con
 	public static WebDriver driver = BaseClass.returnDriver();
 	LoginPage login = new LoginPage(driver);
-	@Test
-	public void login() {	
+	
+	@Test(dataProvider="getData")
+	public void login(String name,String password) {	
 		driver = BaseClass.returnDriver();
 		BaseClass.getURL();
 		driver.manage().window().maximize();
-		login.getUserName().sendKeys("ddd");;
-		login.getPassword().sendKeys("dddd");
-		
-		
-		
+		login.getUserName().sendKeys(name);;
+		login.getPassword().sendKeys(password);
+				
 	}
+	
+	@DataProvider(name="getData")
+	public void getData() {
+		Object [][] data = new Object[1][2];
+		data[0][0] = "Random name";
+		data[0][1] ="random pas";
+		data[1][0]="random name two";
+		data[1][1]="random pass2";
+	}
+	
+	
 	
 }
